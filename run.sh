94007bc06deadfa3807557acf31bbda5dee2ee80
华绪 <wuhuaxu@6ack.com>
2020-05-12 16:06:36 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-JBOSS_HOME=`pwd` bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -server-config=standalone-full.xml -Djboss.node.name=ATHENA_APP -Dbatch=true
+JBOSS_HOME=`pwd` bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 -server-config=standalone-full.xml -Djboss.node.name=ATHENA_APP -Dbatch=true -Dresteasy.preferJacksonOverJsonB=false
